433ab64fb26ad52bbddf51a8369a5ecd9c59295e
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-06-24 02:27:48 +0530

    ui design completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import SideBar from "./components/SideBar";
 import HomePage from "./pages/HomePage";
+import FavoritesPage from "./pages/FavoritesPage";
 
 function App() {
   return (
     <>
       <div className="flex">
         <SideBar />
-        <HomePage />
-        </div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+        </Routes>
+      </div>
     </>
   );
 }
